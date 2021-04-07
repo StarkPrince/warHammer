549f6453f70b48c9918bb000fc928b681ff54570
Prince Raj <games.princeraj@gmail.com>
2021-04-07 10:52:47 +0530

    ignore

--- dealingProbs/cpp/init.cpp
+++ dealingProbs/cpp/init.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <math.h>
 #include <string.h>
-// #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
 int main()
@@ -11,5 +11,28 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    cout <<" cr";
+    int n, k;
+    cin >> n >> k;
+    vector<int> g;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        int q = a[i] / k;
+        for (int j = 1; j <= q; j++)
+        {
+            g.push_back(k * j);
+        }
+        if (a[i] % k != 0)
+            g.push_back(a[i]);
+    }
+    int ans = 0;
+    for (int i = 0; i < g.size(); i++)
+    {
+        int mx = g[i];
+        int mn = (g[i] - 1) / k;
+        mn = mn * k;
+        ans += (i > mn && i <= mx);
+    }
+    cout << ans;
 }
