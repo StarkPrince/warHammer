2a33de342dc7cad0d2d0e7117f9ded7eb3b6836c
Prince Raj <games.princeraj@gmail.com>
2021-06-21 19:46:44 +0530

    idk

---
+++ cpp/dataTypes/noofways.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <limits.h>
+using namespace std;
+
+typedef long long ll;
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+}
