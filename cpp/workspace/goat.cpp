ebb801706cade0a27593ada0cbd61b28032f94e5
StarkPrince <games.princeraj@gmail.com>
2021-10-31 22:18:44 +0530

    real men

--- cpp/workspace/goat.cpp
+++ cpp/workspace/goat.cpp
@@ -27,7 +27,26 @@ const int MOD = 1e9 + 9;
 
 int main()
 {
-    int n;
-    cin >> n;
-    //
+    int N, K;
+    cin >> N >> K;
+    set<vector<int>> vecs;
+    int ans = 0;
+    while (N--)
+    {
+        string S;
+        cin >> S;
+        vector<int> cur(K);
+        for (int i = 0; i < K; i++)
+            cur[i] = S[i] / 3 % 3;
+        for (const auto &v : vecs)
+        {
+            vector<int> cnd(K);
+            for (int i = 0; i < K; i++)
+                cnd[i] = (6 - cur[i] - v[i]) % 3;
+            if (vecs.count(cnd))
+                ans++;
+        }
+        vecs.insert(cur);
+    }
+    cout << ans / 2 << '\n';
 }
--- python/workspace/rough.py
+++ python/workspace/rough.py
@@ -1,15 +1,1 @@
-# ;gitgud [delta=0] [+tags...] [-tags...]
-
-# Gitgud: You can request a problem from the bots relative to your current rating with ;gitgud <delta>
-# - It is also possible to request problems with a certain tag now but you get less points for it: ;gitgud <delta> [+tags...] [-tags...]
-# - After solving the problem you can claim gitgud points for it with ;gotgud
-# - If you can't solve the problem for 2 hours you can skip it with ;nogud
-# - The all-time ranklist can be found with ;gitgudders
-# - A monthly ranklist is shown when you type ;monthlygitgudders
-# - Another way to gather gitgud points is ;upsolve (only works if you have no active gitgud-Challenge)
-# - For help with each of the commands you can type ;help <command> (e.g. ;help gitgudders)
-
-# Point distribution:
-# delta  | -300 | -200 | -100 |  0  | +100 | +200 | +300 | +400 | +500 | +600 | +700
-# no tags|   2  |   3  |   5  |  8  |  12  |  17  |  23  |  23  |  23  |  23  |  23
-# tags   |   2  |   2  |   2  |  3  |   5  |   8  |  12  |  17  |  23  |  23  |  23
+print(ord("s"), ord("e"), ord("t"))
