2e80217f0d281df964ee7134b41005e79a922a78
StarkPrince <games.princeraj@gmail.com>
2021-11-19 00:37:54 +0530

    hrmlo

--- python/workspace/rough.py
+++ python/workspace/rough.py
@@ -1,8 +1,23 @@
-def checkAlmostEquivalent(word1: str, word2: str) -> bool:
-    for i in range(97, 123):
-        if abs(word1.count(chr(i)) - word2.count(chr(i))) > 3:
-            return False
-    return True
+def solve(n, m, mt, nt, k):
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    bs, ans = 0, 0
+    for ai in range(k + 1):
+        try:
+            while b[bs] < a[ai] + mt:
+                bs += 1
+        except IndexError:
+            print(-1)
+            return None
+        bi = k - ai
+        try:
+            if ans < b[bs + bi] + nt:
+                ans = b[bs + bi] + nt
+        except IndexError:
+            print(-1)
+            return None
+    print(ans)
 
 
-print(checkAlmostEquivalent("bbbb", "zzzz"))
+n, m, mt, nt, k = [int(x) for x in input().split()]
+solve(n, m, mt, nt, k)
