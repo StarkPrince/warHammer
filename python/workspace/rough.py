3b6d219c756c0f5ddc92591b13635d520b4c9e4d
StarkPrince <games.princeraj@gmail.com>
2022-01-27 06:31:18 +0530

    i am dumb :(

--- python/workspace/rough.py
+++ python/workspace/rough.py
@@ -1,19 +1,9 @@
-# ugly number
-# find the nth ugly number
-def ugly(n):
-    ugly_list = [1]
-    i2, i3, i5 = 0, 0, 0
-    while len(ugly_list) < n:
-        ugly_list.append(
-            min(ugly_list[i2]*2, ugly_list[i3]*3, ugly_list[i5]*5))
-        if ugly_list[-1] == ugly_list[i2]*2:
-            i2 += 1
-        if ugly_list[-1] == ugly_list[i3]*3:
-            i3 += 1
-        if ugly_list[-1] == ugly_list[i5]*5:
-            i5 += 1
-    return ugly_list[-1]
+def generateMatrix(n):
+    res, lo = [[n*n]], n*n
+    while lo > 1:
+        lo, hi = lo - len(res), lo
+        res = [[i for i in range(lo, hi)]] + [list(j) for j in zip(*res[::-1])]
+    return res
 
 
-print(ugly(10))
-print(ugly(1500))
+print(generateMatrix(3))
